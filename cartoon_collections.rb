728fd2c7e023d1c451e59f2bfb9a142121d16b2e
Sashin Trout <s.ttrout@hotmail.com>
2020-08-09 20:34:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,9 @@ def long_planeteer_calls(array)
 end
 end
 
-def find_the_cheese(array)# code an argument here
+def find_the_cheese(array)
   array.find do |cheese|
-  cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
 end
+
